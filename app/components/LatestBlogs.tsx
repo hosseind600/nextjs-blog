4cf06126ca065adf78d7d859f6915e942e148785
hosseind600 <hosseind600@gmail.com>
2023-12-10 00:54:57 +0330

    Added latest blogs to home

--- app/components/LatestBlogs.tsx
+++ app/components/LatestBlogs.tsx
@@ -1,7 +1,68 @@
 export default function LatestBlogs() {
     return (
-      <div className="px-2  lg:px-20 md:px-10 sm:px-5 ">
-    t4t
+      <div className="px-2  lg:px-20 md:px-10 sm:px-5 bg-gray-800 py-16 relative">
+          <svg fill="#7d31b5" className="bgsvg h-[230px] z-0 absolute w-full top-0 left-0" viewBox="0 0 1000 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"  aria-hidden="true"><path d="M0 100s969.9-9.9 1000-40v40H0z"></path><path d="M0 100S909.5 90.5 1000 0v100H0z" opacity=".5"></path><path d="M0 100s940-10 1000-70v70H0z" opacity=".3"></path></svg>
+          <h2 className="text-center mt-20 relative text-white z-10 text-[40px] font-bold uppercase">Latest Blog Posts</h2>
+          <span className="uppercase relative lead z-10 text-sm font-bold text-center mb-10 block">Tutorials about Webdev & Gamedev</span>
+          <div className="grid relative grid-cols-1 z-10 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+            <div className="bg-gray-700 rounded-lg shadow-md relative overflow-hidden pb-3">
+              <img src="slides/p1.jpg" alt="post1" className="aspect-sqaure postimg mb-3 object-cover  w-full" />
+              <span className="uppercase text-gray-300 ml-3 mb-3">May 11, 2023</span>
+              <h1 className="text-white ml-3 mb-3 font-bold text-lg mr-3">13 Godot Engine Tricks You’ll WISH You Knew Sooner</h1>
+              <p className="text-gray-200 ml-3 mb-3 text-sm text-justify mr-3">When working as a game developer, every tip and trick to save yourself time and make your life easier counts. And I briefly scoured the internets to find some great Godot Engine tricks and bundled them up in a nice quick video for you, so you don’t have to. Here are 12 Godot Engine tricks</p>
+              <a href="#" className="bg-gray-800 rounded-full py-1 px-4 text-sm text-white mt-3 ml-3 cr">Continue Reading...</a>
+            </div>
+
+          </div>
       </div>
     )
   }
