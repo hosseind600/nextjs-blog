db92efa18607868d7d73a6fee65cbce57dc7d0ae
hosseind600 <hosseind600@gmail.com>
2023-12-11 18:33:37 +0330

    Enhanced Responsiveness

--- app/components/RespNav.tsx
+++ app/components/RespNav.tsx
@@ -8,7 +8,7 @@ export default function RespNav({activeTab}) {
     return (
       <div className="md:hidden h-10 aspect-square flex items-center justify-center cursor-pointer ">
             <svg onClick={()=>setIsOpen(!isOpen)} fill="#fff" xmlns="http://www.w3.org/2000/svg" height="22" width="20" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
-            <div className={isOpen ? "md:hidden flex flex-col absolute right-0 top-[85px] w-full p-8 px-10 bg-gray-950/90 rounded shadow-md" : "hidden"}>
+            <div className={isOpen ? "md:hidden flex flex-col absolute right-0 top-[85px] w-full p-8 px-10 bg-gray-950/90 rounded shadow-md z-30" : "hidden"}>
               <li className={activeTab=="home" ? 'text-yellow-500  py-7' :  'py-7'}><a href="/">Home</a></li>
               <li className={activeTab=="downloads" ? 'text-yellow-500  py-7' :  'py-7'}><a href="/downloads/">Downloads</a></li>
               <li className={activeTab=="articles" ? 'text-yellow-500  py-7' :  'py-7'}><a href="/posts/">Articles</a></li>
